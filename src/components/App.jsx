563807662851f4c360d4b1bd51c10692d3eb4ad9
Yulia Kutova <kutova85@gmail.com>
2023-04-19 21:33:08 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
-// import { useState } from "react";
-// import { nanoid } from 'nanoid';
 import { Layout } from './App.styled'
 import { GlobalStyle } from './GlobalStyle';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "redux/operations";
+import { selectError, selectIsLoading } from "redux/selectors";
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
-   
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
     return (
       <Layout>
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
         <Filter />
+        {isLoading && !error && <Loader />}
         <ContactList />
         <GlobalStyle />
       </Layout>
